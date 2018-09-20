8e03ba80c93a992521dfa826128c4062746dd67d
MacBook Pro <macbookpro@MBP-di-MacBook.fritz.box>
2018-09-20 21:08:40 +0200

    networking added

--- networking/basic-fetch/main.js
+++ networking/basic-fetch/main.js
@@ -1,17 +1,52 @@
-let textUrl = '';
-let jsonUrl = '';
-let imageUrl = '';
+let textUrl = 'resources/my-image.jpg';
+let jsonUrl = 'resources/colors.json';
+let imageUrl = 'resources/text';
 
 
 
-function fetchText(textUrl){
-  return fetch().then().then().catch();
+function fetchText(){
+  return fetch(jsonUrl)
+      .then( response => response.text() )
+      .then( myText =>  drawText(myText) )
+      .catch(() => console.log('catched') );
 }
 
-function fetchJson(jsonUrl){
-  return fetch().then().then().catch();
+function fetchJson(){
+  return fetch(jsonUrl)
+      .then( response => response.json() )
+      .then( myJson =>  drawJson(myJson) )
+      .catch(() => console.log('catched') );
 }
 
-function fetchImage(imageUrl){
-  return fetch().then().then().catch();
+function fetchImage(){
+  return fetch(imageUrl)
+        .then( response => response.blob())
+        .then( response => drawImage(response) )
+        .catch( () => console.log('catched') );
 }
+
+function drawImage(bulbImage){
+   var imgElem = document.getElementsByClassName('my-image');
+   var imgUrl = URL.createObjectURL(bulbImage);
+   imgElem.src = imgUrl;
+}
+
+function drawText(text){
+  documnet.getElementsByClassName('my-json').innerHTML=text;
+}
+
+function drawJson(json){
+  documnet.getElementsByClassName('my-json').innerHTML=JSON.stringify(json);
+}
+
+
+function showError(){
+
+}
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     fetchImage(imageUrl);
+//     fetchJson(jsonUrl);
+//     fetchText(textUrl);
+// }, false);
