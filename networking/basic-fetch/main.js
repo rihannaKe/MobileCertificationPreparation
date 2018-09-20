b51a27b73616feeb2208cbbde173789285a20d13
MacBook Pro <macbookpro@MBP-di-MacBook.fritz.box>
2018-09-20 20:05:11 +0200

    networking added

---
+++ networking/basic-fetch/main.js
@@ -0,0 +1,17 @@
+let textUrl = '';
+let jsonUrl = '';
+let imageUrl = '';
+
+
+
+function fetchText(textUrl){
+  return fetch().then().then().catch();
+}
+
+function fetchJson(jsonUrl){
+  return fetch().then().then().catch();
+}
+
+function fetchImage(imageUrl){
+  return fetch().then().then().catch();
+}
